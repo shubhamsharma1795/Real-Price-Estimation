14d533d04dc777aaf6e51752cbf5d02183edf95c
shubhamsharma1795 <154001207+shubhamsharma1795@users.noreply.github.com>
2024-04-20 12:06:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,39 @@
 import streamlit as st
 import numpy as np
 import json
-import gdown
+import requests
 import pickle
 
-# Function to download the JSON file and load it
+# Function to download the JSON file and load it from GitHub
 @st.cache
 def load_column_data():
-    json_url = 'https://drive.google.com/uc?id=1QUYGRnLQdd2v5B6TfQbIOJLGuRZuQpzS'
-    json_path = 'columns.json'
-    gdown.download(json_url, json_path, quiet=False)
+    json_url = 'https://raw.githubusercontent.com/shubhamsharma1795/Real-Price-Estimation/main/columns.json'
+    response = requests.get(json_url)
+    response.raise_for_status()  # Will raise an exception if the download failed
+    data = response.json()
     
-    with open(json_path, 'r') as file:
-        data = json.load(file)
-    
-    # Ensure that the keys exist in the JSON data
     columns = data.get('columns', [])
     area_options = data.get('data_columns', [])
     
     if not columns or not area_options:
         raise ValueError("JSON file is missing 'columns' or 'data_columns' keys")
-
+    
     return columns, area_options
 
 # Function to load the pickled model
 @st.cache(allow_output_mutation=True)
 def load_pickled_model():
-    model_url = 'https://drive.google.com/uc?id=1OMFIXo38RsVSbCYC8_PEyAEYYdTJq7qt'
-    model_path = 'bangalore_home_price_model.pickle'
-    gdown.download(model_url, model_path, quiet=False)
+    # You need to provide the URL where your pickled model is stored
+    model_url = 'https://drive.google.com/uc?id=1OMFIXo38RsVSbCYC8_PEyAEYYdTJq7qt'  # Update this URL
+    model_path = 'banglore_home_price_model.pickle'
+    response = requests.get(model_url)
+    with open(model_path, 'wb') as f:
+        f.write(response.content)
     with open(model_path, 'rb') as f:
         model = pickle.load(f)
     return model
 
+# Load your model (assuming URL is provided correctly)
 model = load_pickled_model()
 columns, area_options = load_column_data()
 
